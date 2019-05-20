180c5b751b01ed2d6e2085104fa0c10eadf130e0
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-20 15:43:31 +0300

    Added a simple router functionality for home, contact and 404 setup.. setup...

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import 	(
  )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-type", "text/html")
+	if(r.URL.Path == "/"){
+		fmt.Fprint(w, "<h1>Hey dog lover! Welcome!</h1>")
+	} else if(r.URL.Path == "/contact") {
+		fmt.Fprint(w, "To get in touch, please drop us a line at: <a href =\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>")
+	} else {
+		fmt.Fprint(w, "Ooops... it appears we don't have this page arround.")
+	}
 }
 
 func main()  {
