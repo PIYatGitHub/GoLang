f8e1ab7ac1fb94c4ad601b43247d424d9658fedb
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-20 15:51:41 +0300

    updated 404...

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import 	(
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-type", "text/html")
 	if(r.URL.Path == "/"){
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "<h1>Hey dog lover! Welcome!</h1>")
 	} else if(r.URL.Path == "/contact") {
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "To get in touch, please drop us a line at: <a href =\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>")
 	} else {
-		fmt.Fprint(w, "Ooops... it appears we don't have this page arround.")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Ooops... it appears we don't have this page arround. Imagine a 404 here... ")
 	}
 }
 
