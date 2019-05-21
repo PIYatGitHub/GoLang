abe229a42214fc7a991f4c1ebe35fa6065efb77c
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-21 22:34:13 +0300

    eod: did some more templates... more --> tomorrow

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
