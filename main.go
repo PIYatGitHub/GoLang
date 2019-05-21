de76ff76589a4fb112b861676ae27379c11480d6
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-21 19:42:54 +0300

    May there be views... created a folder for the views and added two of them to handle what was previously returned as strings in the handler functions

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "<h1>Hey dog lover! Welcome!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "To get in touch, please drop us a line at: <a href =\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
