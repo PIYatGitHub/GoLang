e8dcd4051759849fbebff92a16e7db4c94a0cfbb
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-21 10:37:02 +0300

    2 handler funcs

--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
+
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	if r.URL.Path == "/" {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "<h1>Hey dog lover! Welcome!</h1>")
-	} else if r.URL.Path == "/contact" {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "To get in touch, please drop us a line at: <a href =\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "Ooops... it appears we don't have this page arround. Imagine a 404 here... ")
-	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "<h1>Hey dog lover! Welcome!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "To get in touch, please drop us a line at: <a href =\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
-	// http.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", r)
 }
