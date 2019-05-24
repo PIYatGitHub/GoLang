030c0a29d89f2bad24eea701fc998e1369f23d41
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-24 14:52:29 +0300

    refactoring - done for now

--- views/view.go
+++ views/view.go
@@ -1,13 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 // NewView is a function that creates and returns the new view with all the default files included
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/navbar.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/footer.gohtml")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 
 	if err != nil {
@@ -25,4 +30,13 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of strings to represent all layout files
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
