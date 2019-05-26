24b8a22797b58811f1e5ac3734ac9571aed4804a
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-26 17:56:33 +0300

    improoved comments in the users.go file

--- controllers/users.go
+++ controllers/users.go
@@ -7,7 +7,8 @@ import (
 	"../views"
 )
 
-// NewUser creates a new user - capt. obvious strikes again!!!
+// NewUser creates a new user view - capt. obvious strikes again!!!
+// This function shall panic if there is some err.
 func NewUser() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
@@ -20,12 +21,14 @@ type Users struct {
 }
 
 // New  --> Use to render the form to create a new user!
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
 // Create is called whenever you submit the form ... se we create
-// a new user here...
+// a new user account here...
+// POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is a fake passage. Pretend we created a user account...")
 }
