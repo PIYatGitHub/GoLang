b655b3ce6beea16d86b0a115558d038d7e5feada
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-26 13:12:43 +0300

    refactoring - deno + using the first controller - done

--- controllers/users.go
+++ controllers/users.go
@@ -1,10 +1,24 @@
 package controllers
 
+import (
+	"net/http"
+
+	"../views"
+)
+
 // NewUser creates a new user - capt. obvious strikes again!!!
 func NewUser() *Users {
-	return &Users{}
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
 }
 
 //Users is a users struct!!!
 type Users struct {
+	NewView *views.View
+}
+
+// New  --> Use to render the form to create a new user!
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	u.NewView.Render(w, nil)
 }
