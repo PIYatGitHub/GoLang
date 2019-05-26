aa32e6979c8dfa6e388bec2a5d1586277fe64744
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-26 12:09:31 +0300

    added first controller to our APP

---
+++ controllers/users.go
@@ -0,0 +1,10 @@
+package controllers
+
+// NewUser creates a new user - capt. obvious strikes again!!!
+func NewUser() *Users {
+	return &Users{}
+}
+
+//Users is a users struct!!!
+type Users struct {
+}
