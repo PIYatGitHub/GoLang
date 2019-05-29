5f0cd613b5d0ffb0de8b9dc3169c59cfed60f515
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-29 19:49:17 +0300

    adding some infrastructure

--- models/users.go
+++ models/users.go
@@ -2,6 +2,16 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// UserService holds the logic?
+type UserService struct {
+}
+
+//ByID will lookup the user by id;
+// it will return user,nil or nil for the user and specific user (only one)
+func (us *UserService) ByID(id uint) (*User, error) {
+
+}
+
 // User will serve to save our users with the appropriate fields...
 type User struct {
 	gorm.Model
