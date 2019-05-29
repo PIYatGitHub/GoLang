361e7d5c4a29a58fee2a678445fa6c019e414da2
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-29 20:08:47 +0300

    added a useful tool - DB resetter

--- models/users.go
+++ models/users.go
@@ -47,6 +47,12 @@ func (us *UserService) Close() error {
 	return us.db.Close()
 }
 
+//DestructiveReset deletes the users table. NEVER EVER RUN IN PROD!!!!!
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User)
+	us.db.AutoMigrate(&User{})
+}
+
 // User will serve to save our users with the appropriate fields...
 type User struct {
 	gorm.Model
