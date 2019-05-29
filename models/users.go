e564cf0eccc402f178ff756392b680eafe2b4cbf
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-29 20:01:00 +0300

    infrastructure is in place... we can search by Id, we handle errors, we have open and close methods for the DB conn.

--- models/users.go
+++ models/users.go
@@ -1,15 +1,50 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	//ErrNotFound is returned whenever you cannot find the resource at the DB
+	ErrNotFound = errors.New("models:resource not found")
+)
+
+// NewUserService will open a singular connection to the DB!
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+}
 
 // UserService holds the logic?
 type UserService struct {
+	db *gorm.DB
 }
 
 //ByID will lookup the user by id;
 // it will return user,nil or nil for the user and specific user (only one)
 func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
 
+// Close will terminate the connection to the DB!
+func (us *UserService) Close() error {
+	return us.db.Close()
 }
 
 // User will serve to save our users with the appropriate fields...
