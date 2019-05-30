c4a09856978c04c2713dfbac562507d2dcd6f98f
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-05-30 08:26:24 +0300

    added the user service support to the main.go file

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"./models"
 
 	"../lenslocked.com/controllers"
 	"github.com/gorilla/mux"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "lenslocked_dev"
+)
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port =%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUser()
+	usersC := controllers.NewUser(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
